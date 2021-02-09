411cf41b5378b2e0a9a3955917605c05537588fc
VictorCoboCopernic <cobo.jandula.victor@alumnat.copernic.cat>
2021-02-09 17:00:31 +0100

    test php

---
+++ test.php
@@ -0,0 +1,74 @@
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/Style.css"/>
+        <link href="http://pheist.net/fonts.php?id=51" rel="stylesheet">
+        <link rel="shortcut icon" href="Visuals/ADII.ico" />
+        <title>ADII</title>
+      
+        <div class="hed">
+          
+            <a href="Index.html"> <img src="Visuals/ADII.png" class="logo" style="width: 200"> </a>
+            <ul>
+                <li><a href="acerca.html">Acerca de ADII</a></li>
+                <li><a><input type="text" placeholder="Introducir codigo" id="nuevoTexto"></a></li>
+                <li><a href=".html">Registro</a></li>
+                <li><a href=.html>Login</a></li>
+            </ul>
+          
+        </div>
+       
+    </head>
+
+    <body class="body"> 
+      
+        <div class="bod">
+             
+            <table>
+                <tr>
+                    <td><button onclick="selecopcion(1, 1)">OPCION 1</button></td>
+                    <td><button onclick="selecopcion(1, 2)">OPCION 2</button></td>
+                    <td><button onclick="selecopcion(1, 3)">OPCION 3</button></td>
+                    <td><button onclick="selecopcion(1, 4)">OPCION 4</button></td>
+                </tr>
+                <tr>
+                    <td><button onclick="selecopcion(2, 1)">OPCION 1</button></td>
+                    <td><button onclick="selecopcion(2, 2)">OPCION 2</button></td>
+                    <td><button onclick="selecopcion(2, 3)">OPCION 3</button></td>
+                    <td><button onclick="selecopcion(2, 4)">OPCION 4</button></td>
+                </tr>
+                <tr>
+                    <td><button onclick="selecopcion(3, 1)">OPCION 1</button></td>
+                    <td><button onclick="selecopcion(3, 2)">OPCION 2</button></td>
+                    <td><button onclick="selecopcion(3, 3)">OPCION 3</button></td>
+                    <td><button onclick="selecopcion(3, 4)">OPCION 4</button></td>
+                </tr>
+                <tr>
+                    <td><button onclick="selecopcion(4, 1)">OPCION 1</button></td>
+                    <td><button onclick="selecopcion(4, 2)">OPCION 2</button></td>
+                    <td><button onclick="selecopcion(4, 3)">OPCION 3</button></td>
+                    <td><button onclick="selecopcion(4, 4)">OPCION 4</button></td>
+                </tr>
+            </table>
+   
+            <br>
+            <br>
+            <br>
+        </div>
+
+        
+      
+        <footer>
+            <div class="fut"></div>  
+        </footer>
+        
+        
+
+
+
+        
+
+        
+        
+    </body>  
+</html>
